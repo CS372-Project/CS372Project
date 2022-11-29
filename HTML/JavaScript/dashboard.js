9ffb0dd7b84ad3a860d6c5fafe2729927a47cd59
Yi-Shain Liu <liuy13@pfw.edu>
2022-11-28 20:55:35 -0500

    Add profile image upload fuction

--- HTML/JavaScript/dashboard.js
+++ HTML/JavaScript/dashboard.js
@@ -3,3 +3,25 @@ $(document).ready(function () {
         $('#sidebar').toggleClass('active');
     });
 });
+function clickPicture(){
+    let file = document.getElementById("file")
+    file.click()
+}
+function previewFile(){
+    console.log("debug1")
+    var preview = document.querySelector('img');
+    var file    = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
+
+    reader.addEventListener("load", function () {
+         preview.src = reader.result;
+    }, false);
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
+
+// $("img[id='photo-image1']").click(function() {
+//     $("input[id='file']").click();
+// });

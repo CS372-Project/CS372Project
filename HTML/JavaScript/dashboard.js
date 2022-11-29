f1333137cd62609e82518a093597b11cbab3620c
Yi-Shain Liu <liuy13@pfw.edu>
2022-11-28 23:02:23 -0500

    Fix

--- HTML/JavaScript/dashboard.js
+++ HTML/JavaScript/dashboard.js
@@ -4,13 +4,12 @@ $(document).ready(function () {
     });
 });
 function clickPicture(){
-    let file = document.getElementById("file")
-    file.click()
+    let file = document.getElementById("file");
+    file.click();
 }
 function previewFile(){
-    console.log("debug1")
     var preview = document.querySelector('img');
-    var file    = document.querySelector('input[type=file]').files[0];
+    var file = document.querySelector('input[type=file]').files[0];
     var reader  = new FileReader();
 
     reader.addEventListener("load", function () {

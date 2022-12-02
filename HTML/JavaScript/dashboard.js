ef445bf8646942b099c168ede21ab4048f1fa610
Yi-Shain Liu <liuy13@pfw.edu>
2022-12-01 19:33:18 -0500

    testing

--- HTML/JavaScript/dashboard.js
+++ HTML/JavaScript/dashboard.js
@@ -20,3 +20,19 @@ function previewFile(){
         reader.readAsDataURL(file);
     }
 }
+function getAllGameQuiz(){
+    let games = [{title:"AGame", link:"https://www.google.com/", creator:"Mr. A"},{title:"BGame", link:"https://www.youtube.com/", creator:"Mr. B"}];
+    let contener = document.getElementById("gameQuiz");
+    for (let index = 0; index < games.length; index++) {
+        const element = games[index];
+        let oneGame = document.createElement("div");
+        let title = document.createElement("a");
+        title.innerText = element.title;
+        title.href = element.link;
+        oneGame.appendChild(title);
+        let creator = document.createElement("h4");
+        creator.innerText = element.creator;
+        oneGame.appendChild(creator);
+        contener.appendChild(oneGame);
+    }
+}

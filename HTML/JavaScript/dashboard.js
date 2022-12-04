336fdbf63b8ad4a3c0361e15affda5f7ac5ffd10
Yi-Shain Liu <89481395+YiShianLiu@users.noreply.github.com>
2022-12-04 15:49:00 -0500

    Improve dashboard (#47)
    * Improve dashboard
    
    * try to solve mocha
    
    * solve mocha
    
    * remove .env
    
    * final change for dashboard
    
    * temperory add .env so the test can pass

--- HTML/JavaScript/dashboard.js
+++ HTML/JavaScript/dashboard.js
@@ -20,3 +20,40 @@ function previewFile(){
         reader.readAsDataURL(file);
     }
 }
+function getAllGameQuiz(){
+    console.log("debugTest");
+    $.ajax({
+        type: "POST",
+        url: "dashboard",
+        error: function(req, err){
+            console.log(err)
+        },
+        success: function(data) {
+            console.log("debugTest");
+            console.log(data);
+            let games = data;
+            let contener = document.getElementById("gameQuiz");
+            for (let index = 0; index < games.length; index++) {
+                const element = games[index];
+                let oneGame = document.createElement("div");
+                let button = document.createElement("form");
+                button.setAttribute("method","post");
+                button.setAttribute("action","/play");
+                let input = document.createElement("input");
+                input.setAttribute("type", "hidden");
+                input.setAttribute("name", "title");
+                input.setAttribute("value", element.title);
+                let title = document.createElement("a");
+                title.setAttribute("onclick", "this.parentNode.submit();");
+                title.innerText = element.title;
+                button.appendChild(input);
+                button.appendChild(title);
+                oneGame.appendChild(button);
+                let creator = document.createElement("h4");
+                creator.innerText = element.creator;
+                oneGame.appendChild(creator);
+                contener.appendChild(oneGame);
+            }
+        },
+    })
+}

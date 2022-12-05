a2c722d487910dd6bf67e4ec00ecc1ee085aab74
NICK <nickscheiber@hotmail.com>
2022-12-04 19:31:41 -0500

    adding play functionality

--- HTML/JavaScript/play.js
+++ HTML/JavaScript/play.js
@@ -7,3 +7,48 @@ const validateGuestForm = () => {
 
   };
 
+const mongoose = require('mongoose');
+
+const QuestionSchema = new mongoose.Schema({
+  question_type: String,
+  question_text: String,
+  question_possibilities: [{
+    answer: String
+  }],
+  correct_answer: String,
+  updated_at: { type: Date, default: Date.now },
+});
+
+const Question = mongoose.model('Question', QuestionSchema);
+
+module.exports = {
+  Question,
+  getCorrectAnswers,
+  getQuizAnswers,
+}
+
+function getCorrectAnswers() {
+  return Question.find()
+  .then(res => {
+    return res.reduce((agg, q) => {
+      agg[q._id] = q.correct_answer;
+      return agg;
+  }, {});
+  });
+}
+
+function getQuizQuestions() {
+  return Question.find()
+  .then(res => {
+    return res.map(q => {
+      const data = q.toObject();
+      const answer = data.correct_answer;
+      delete data.correct_answer;
+      data.question_possibilities.push({answer})
+      data.question_possibilities = data.question_possibilities
+      .map(d => ({answer: d.answer}));
+      return data;
+    })
+
+  });
+}
